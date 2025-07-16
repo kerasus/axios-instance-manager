530bcbcbe97c6aa45af22ec84ddaea8734405bd4
ali <a.esmaeili@asredanesh.com>
2025-07-16 13:15:23 +0330

    fix: expose AuthenticatedMiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as AxiosInstanceManager } from './AxiosInstanceManager';
 export * from './AxiosInstanceManager';
+export * from './AuthenticatedMiddleware';
 export * from './jwtUtils';
