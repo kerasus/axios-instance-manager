d3dbf0dc0e5c84bda81ae2153d78b1449186d96b
ali <a.esmaeili@asredanesh.com>
2025-09-13 17:21:06 +0330

    fix: export index.ts

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import path from 'path'
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'AxiosInstanceManager',
+            formats: ['es', 'cjs'],
+            fileName: (format) => `index.${format}.js`,
+        },
+        rollupOptions: {
+            external: ['vue', 'axios', 'vue-router'],
+            output: {
+                globals: {
+                    vue: 'Vue',
+                    axios: 'axios',
+                    'vue-router': 'VueRouter',
+                },
+            },
+        },
+    },
+    plugins: [
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
+})
